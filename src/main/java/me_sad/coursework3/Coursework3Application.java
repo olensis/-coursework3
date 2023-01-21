31cb19ccedebff88b48db54cb552812dafd196f5
olensis <olensis7gmail@.com>
2023-01-21 10:43:58 +0300

    1 комит

---
+++ src/main/java/me_sad/coursework3/Coursework3Application.java
@@ -0,0 +1,13 @@
+package me_sad.coursework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework3Application.class, args);
+    }
+
+}
